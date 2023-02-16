dd72a6c365f30271f938510a96cd9df43a917eaa
Annet-Chebukati <annetnasimiyuchebukati@gmail.com>
2023-02-16 18:11:29 +0000

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Prits required text to the standard error
+ *
+ * Return: 1 to indicate ana error occured
+ */
+int main(void)
+{
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, sizeof(message) - 1);
+	return (1);
+}
