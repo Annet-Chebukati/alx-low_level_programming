4eeb33e6bc14490156e3932d2fc415d580344939
Annet-Chebukati <annetnasimiyuchebukati@gmail.com>
2023-02-16 14:37:06 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints string to the console
+ *
+ * Return: Always 0 (Success)
+ */             
 int main(void)
 {
-	puts("Programming is like building a muiltilingual puzzle");
+	puts("\"Programming is like building a muiltilingual puzzle");
 	return (0);
 }
