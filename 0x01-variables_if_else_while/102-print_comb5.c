678c5c18adc22c14928f86a27470cba441dca27f
Annet-Chebukati <annetnasimiyuchebukati@gmail.com>
2023-02-17 15:26:45 +0000

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,12 +23,16 @@ int main(void)
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
-			if (i < 99 || j < 99)
+			if (i == 98 && j == 99)
 			{
-				putchar(',');
-				putchar(' ');
+				break;
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
+	putchar('\n');
+
 	return (0);
 }
