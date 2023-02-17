12eb1ce623ad0c7698c4a17e8bbf21f3bbdfeab0
Annet-Chebukati <annetnasimiyuchebukati@gmail.com>
2023-02-17 14:36:57 +0000

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Outer loop iterates over all the possible first numbers
+ * Inner loop iterates over all possible second numbers
+ * Putchar function prints each digit of the two numbers,
+ * as well as separating space and commas
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i < 99 || j < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
