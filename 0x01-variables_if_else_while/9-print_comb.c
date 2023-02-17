3a88fa0f4e5316a1a3c8f6fd0aa404cb37a65bef
Annet-Chebukati <annetnasimiyuchebukati@gmail.com>
2023-02-17 13:18:56 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,20 +13,17 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i; j < 10; j++)
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
-			putchar(i + '0');
-			if (i != 9 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
+		
 	}
 
 	putchar('\n');
