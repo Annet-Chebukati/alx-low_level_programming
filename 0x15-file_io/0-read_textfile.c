24bc475541ad393a4161c3cd6626b6818272b267
Annet-Chebukati <annetnasimiyuchebukati@gmail.com>
2023-04-08 04:19:19 -0700

    reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,19 +17,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		return (-1);
+		return (0);
 	}
 
 	buff = malloc(sizeof(char *) * letters);
 	if (buff == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 
 	bytes_read = read(fd, buff, letters);
@@ -37,7 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(buff);
 		close(fd);
-		return (-1);
+		return (0);
 	}
 
 	bytes_written = write(STDOUT_FILENO, buff, bytes_read);
