f83a8b8769d9fb22fc3929d6b095bbeca1c580fd
Annet-Chebukati <annetnasimiyuchebukati@gmail.com>
2023-04-08 04:04:20 -0700

    reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+  * read_textfile - reads a text file and prints it to the
+  * POSIX standard output
+  * @filename: The source file
+  * @letters: Number of letters to reads and prints
+  *
+  * Return: written
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, bytes_read;
+	char *buff;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	buff = malloc(letters);
+	if (buff == NULL)
+	{
+		return (-1);
+	}
+
+	bytes_read = read(fd, buff, letters);
+	if (bytes_read == -1)
+	{
+		free(buff);
+		close(fd);
+		return (-1);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buff, bytes_read);
+
+	free(buff);
+	close(fd);
+
+	return (bytes_written);
+}
